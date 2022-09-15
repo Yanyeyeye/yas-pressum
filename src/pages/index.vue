39161e8a8307c672262fcfc9d20fbfe4af15a42f
Yanyeyeye <1024079655@qq.com>
2022-09-15 12:56:58 +0800

    feat: enable pages auto impot

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+const router = useRouter()
+const go = () => {
+  router.push('/HelloWord')
+}
+</script>
+
 <template>
   <h1>Hello World</h1>
+  <button btn m-3 @click="go">
+    Go
+  </button>
 </template>
